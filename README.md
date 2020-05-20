3d604f022fa0473bb57d72ebb9b6f902e3f7e2ad
tcallred <taylor.allred21@gmail.com>
2020-05-20 17:07:22 -0600

    blue circle now moves down slowly

---
+++ README.md
@@ -0,0 +1,6 @@
+## Rust physics
+
+### TODO
+[X] Get a circle to display with ggez
+[] Apply physics to circle and have it fall
+[] Create a ground for circle to land on
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
+use ggez::nalgebra as na;
+
+const BLUE: graphics::Color = graphics::Color {
+    r: 0.01,
+    g: 0.33,
+    b: 0.98,
+    a: 1.0
+};
+
+struct MyGame {
+    pos_x: f32,
+    pos_y: f32,
+}
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        // Load/create resources such as images here.
+        MyGame {
+            pos_x: 300.0,
+            pos_y: 300.0
+        }
+    }
+}
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        self.pos_y += 0.5;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        // Draw code here...
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            na::Point2::new(self.pos_x, self.pos_y),
+            25.0,
+            0.01,
+            BLUE,
+        )?;
+        graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+        graphics::present(ctx)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Make a Context.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut my_game = MyGame::new(&mut ctx);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e)
+    }
 }
